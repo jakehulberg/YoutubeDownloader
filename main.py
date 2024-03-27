74bfc1bc73c02c8a8d3492205762cecef2a4cab4
jakehulberg <149437250+jakehulberg@users.noreply.github.com>
2024-03-27 18:54:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 from pytube import YouTube
+
 url = input("Youtube URL: ")
+folder = "Youtube_downloads"
 
 my_video = YouTube(url)
 
@@ -9,7 +11,7 @@ print(my_video.title)
 
 my_video = my_video.streams.get_highest_resolution()
 
-my_video.download('/Users/jakehulberg/Desktop/')
+my_video.download('/Users/jakehulberg/Desktop/' + folder)
 
 url2 = input ("2nd Youtube URL: ")
 
@@ -20,7 +22,7 @@ print(my_video2.title)
 
 my_video2 = my_video2.streams.get_highest_resolution()
 
-my_video2.download('/Users/jakehulberg/Desktop/Youtube_downloads')
+my_video2.download('/Users/jakehulberg/Desktop/' + folder)
 
 url3 = input ("3rd Youtube URL: ")
 
@@ -31,4 +33,15 @@ print(my_video3.title)
 
 my_video3 = my_video3.streams.get_highest_resolution()
 
-my_video3.download('/Users/jakehulberg/Desktop/Youtube_downloads')
+my_video3.download('/Users/jakehulberg/Desktop/' + folder)
+
+url4 = input ("4rth Youtube URL: ")
+
+my_video4 = YouTube(url3)
+
+print(my_video4.title)
+
+
+my_video4 = my_video4.streams.get_highest_resolution()
+
+my_video4.download('/Users/jakehulberg/Desktop/' + folder)
